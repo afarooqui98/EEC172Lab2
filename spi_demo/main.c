40ab255d8ad23393c74d0d258e3ce78d781fe5a1
Ahmed Farooqui <farooqui.f.ahmed@gmail.com>
2020-01-23 13:33:43 -0800

    init

---
+++ spi_demo/main.c
@@ -0,0 +1,414 @@
+//*****************************************************************************
+//
+// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
+// 
+// 
+//  Redistribution and use in source and binary forms, with or without 
+//  modification, are permitted provided that the following conditions 
+//  are met:
+//
+//    Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimer.
+//
+//    Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the 
+//    documentation and/or other materials provided with the   
+//    distribution.
+//
+//    Neither the name of Texas Instruments Incorporated nor the names of
+//    its contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// Application Name     - SPI Demo
+// Application Overview - The demo application focuses on showing the required 
+//                        initialization sequence to enable the CC3200 SPI 
+//                        module in full duplex 4-wire master and slave mode(s).
+//
+//*****************************************************************************
+
+
+//*****************************************************************************
+//
+//! \addtogroup SPI_Demo
+//! @{
+//
+//*****************************************************************************
+
+// Standard includes
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Driverlib includes
+#include "hw_types.h"
+#include "hw_memmap.h"
+#include "hw_common_reg.h"
+#include "hw_ints.h"
+#include "spi.h"
+#include "rom.h"
+#include "rom_map.h"
+#include "utils.h"
+#include "prcm.h"
+#include "uart.h"
+#include "interrupt.h"
+
+// Common interface includes
+#include "i2c_if.h"
+#include "uart_if.h"
+#include "pin_mux_config.h"
+
+//OLED interfaces
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1351.h"
+#include "glcdfont.h"
+#include "test.h"
+
+
+#define APPLICATION_VERSION     "1.4.0"
+//*****************************************************************************
+//
+// Application Master/Slave mode selector macro
+//
+// MASTER_MODE = 1 : Application in master mode
+// MASTER_MODE = 0 : Application in slave mode
+//
+//*****************************************************************************
+#define MASTER_MODE      1
+
+#define SPI_IF_BIT_RATE  100000
+#define TR_BUFF_SIZE     100
+
+#define MASTER_MSG       "This is CC3200 SPI Master Application\n\r"
+#define SLAVE_MSG        "This is CC3200 SPI Slave Application\n\r"
+
+//*****************************************************************************
+//                 GLOBAL VARIABLES -- Start
+//*****************************************************************************
+static unsigned char g_ucTxBuff[TR_BUFF_SIZE];
+static unsigned char g_ucRxBuff[TR_BUFF_SIZE];
+static unsigned char ucTxBuffNdx;
+static unsigned char ucRxBuffNdx;
+
+#if defined(ccs)
+extern void (* const g_pfnVectors[])(void);
+#endif
+#if defined(ewarm)
+extern uVectorEntry __vector_table;
+#endif
+
+
+//MACROS
+#define UART_PRINT              Report
+#define FAILURE                 -1
+#define SUCCESS                 0
+#define RETERR_IF_TRUE(condition) {if(condition) return FAILURE;}
+#define RET_IF_ERR(Func)          {int iRetVal = (Func); \
+                                   if (SUCCESS != iRetVal) \
+                                     return  iRetVal;}
+//*****************************************************************************
+//                 GLOBAL VARIABLES -- End
+//*****************************************************************************
+
+//*****************************************************************************
+//
+//! Display the buffer contents over I2C
+//!
+//! \param  pucDataBuf is the pointer to the data store to be displayed
+//! \param  ucLen is the length of the data to be displayed
+//!
+//! \return none
+//!
+//*****************************************************************************
+void
+DisplayBuffer(unsigned char *pucDataBuf, unsigned char ucLen)
+{
+    unsigned char ucBufIndx = 0;
+    UART_PRINT("Read contents");
+    UART_PRINT("\n\r");
+    while(ucBufIndx < ucLen)
+    {
+        UART_PRINT(" 0x%x, ", pucDataBuf[ucBufIndx]);
+        ucBufIndx++;
+        if((ucBufIndx % 8) == 0)
+        {
+            UART_PRINT("\n\r");
+        }
+    }
+    UART_PRINT("\n\r");
+}
+
+
+//*****************************************************************************
+//
+//! SPI Slave Interrupt handler
+//!
+//! This function is invoked when SPI slave has its receive register full or
+//! transmit register empty.
+//!
+//! \return None.
+//
+//*****************************************************************************
+static void SlaveIntHandler()
+{
+    unsigned long ulRecvData;
+    unsigned long ulStatus;
+
+    ulStatus = MAP_SPIIntStatus(GSPI_BASE,true);
+
+    MAP_SPIIntClear(GSPI_BASE,SPI_INT_RX_FULL|SPI_INT_TX_EMPTY);
+
+    if(ulStatus & SPI_INT_TX_EMPTY)
+    {
+        MAP_SPIDataPut(GSPI_BASE,g_ucTxBuff[ucTxBuffNdx%TR_BUFF_SIZE]);
+        ucTxBuffNdx++;
+    }
+
+    if(ulStatus & SPI_INT_RX_FULL)
+    {
+        MAP_SPIDataGetNonBlocking(GSPI_BASE,&ulRecvData);
+        g_ucTxBuff[ucRxBuffNdx%TR_BUFF_SIZE] = ulRecvData;
+        Report("%c",ulRecvData);
+        ucRxBuffNdx++;
+    }
+}
+
+//*****************************************************************************
+//
+//! SPI Master mode main loop
+//!
+//! This function configures SPI modelue as master and enables the channel for
+//! communication
+//!
+//! \return None.
+//
+//*****************************************************************************
+int MasterMain()
+{
+    printf("in master main\n");
+
+
+    //
+    // Set Tx buffer index
+    //
+    ucTxBuffNdx = 0;
+    ucRxBuffNdx = 0;
+
+    //
+    // Reset SPI
+    //
+    MAP_SPIReset(GSPI_BASE);
+
+    //
+    // Configure SPI interface
+    //
+    MAP_SPIConfigSetExpClk(GSPI_BASE,MAP_PRCMPeripheralClockGet(PRCM_GSPI),
+                     SPI_IF_BIT_RATE,SPI_MODE_MASTER,SPI_SUB_MODE_0,
+                     (SPI_SW_CTRL_CS |
+                     SPI_4PIN_MODE |
+                     SPI_TURBO_OFF |
+                     SPI_CS_ACTIVELOW |
+                     SPI_WL_8));
+
+    //
+    // Enable SPI for communication
+    // Enable I2C
+    MAP_SPIEnable(GSPI_BASE);
+    I2C_IF_Open(I2C_MASTER_MODE_FST);
+
+    Adafruit_Init();
+
+    unsigned char ucDevAddr, ucRegOffset, ucRdLen;
+    unsigned char dataBufXPos[256];
+    unsigned char dataBufYPos[256];
+
+
+    ucDevAddr = 0x18;
+    ucRdLen = 1;
+
+    //main loop
+    //position setup
+    volatile int xPos, yPos, xOld, yOld, xOffset, yOffset;
+    volatile int speed = 5;
+    int atEdge = false;
+
+    //set to center
+    xPos = 64;
+    yPos = 64;
+
+    fillScreen(BLACK);
+    drawCircle(xPos, yPos, 2, BLUE);
+    while(1){
+        ucRegOffset = 0x3;
+        RET_IF_ERR(I2C_IF_Write(ucDevAddr, &ucRegOffset, 1, 0));
+        RET_IF_ERR(I2C_IF_Read(ucDevAddr, &dataBufXPos[0], ucRdLen));
+        DisplayBuffer(dataBufXPos, ucRdLen);
+
+        ucRegOffset = 0x5;
+        RET_IF_ERR(I2C_IF_Write(ucDevAddr, &ucRegOffset, 1, 0));
+        RET_IF_ERR(I2C_IF_Read(ucDevAddr, &dataBufYPos[0], ucRdLen));
+        DisplayBuffer(dataBufYPos, ucRdLen);
+
+        int xDist = (int)dataBufXPos[0];
+        int yDist = (int)dataBufYPos[0];
+
+        //store old value to draw over with
+        xOld = xPos;
+        yOld = yPos;
+
+
+        //compute new yOffset based on i2c
+        float ySpeed;
+        if(yDist > 120){
+            ySpeed = -1 * abs(256 - yDist)/60.0;
+        } else if (yDist < 120){
+            ySpeed = (yDist/60.0);
+        }
+
+        yOffset = (int)ySpeed * speed;
+
+        //compute new xOffset based on i2c
+        float xSpeed;
+        if(xDist > 120){
+            xSpeed = -1 * abs(256 - xDist)/60.0;
+        } else if (xDist < 120){
+            xSpeed = (xDist/60.0);
+        }
+
+        xOffset = (int)xSpeed * speed;
+
+//        xOffset = xDist - xOld;
+//        yOffset = yDist - yOld;
+        yPos += yOffset;
+        xPos += xOffset;
+
+        if(xPos < 2 || xPos > 126){
+            xPos = xOld;
+        }
+
+        if(yPos < 2 || yPos > 126){
+            yPos = yOld;
+        }
+
+        drawCircle(yOld, xOld, 2, BLACK);
+        drawCircle(yPos, xPos, 2, BLUE);
+    }
+
+    return SUCCESS;
+}
+
+
+//*****************************************************************************
+//
+//! Board Initialization & Configuration
+//!
+//! \param  None
+//!
+//! \return None
+//
+//*****************************************************************************
+static void
+BoardInit(void)
+{
+/* In case of TI-RTOS vector table is initialize by OS itself */
+#ifndef USE_TIRTOS
+  //
+  // Set vector table base
+  //
+#if defined(ccs)
+    MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
+#endif
+#if defined(ewarm)
+    MAP_IntVTableBaseSet((unsigned long)&__vector_table);
+#endif
+#endif
+    //
+    // Enable Processor
+    //
+    MAP_IntMasterEnable();
+    MAP_IntEnable(FAULT_SYSTICK);
+
+    PRCMCC3200MCUInit();
+}
+
+//*****************************************************************************
+//
+//! Main function for spi demo application
+//!
+//! \param none
+//!
+//! \return None.
+//
+//*****************************************************************************
+void main()
+{
+    //
+    // Initialize Board configurations
+    //
+    BoardInit();
+
+    //
+    // Muxing UART and SPI lines.
+    //
+    PinMuxConfig();
+
+    //
+    // Enable the SPI module clock
+    //
+    MAP_PRCMPeripheralClkEnable(PRCM_GSPI,PRCM_RUN_MODE_CLK);
+
+    //
+    // Initialising the Terminal.
+    //
+    InitTerm();
+
+    //
+    // Clearing the Terminal.
+    //
+    ClearTerm();
+
+    //
+    // Display the Banner
+    //
+    Message("\n\n\n\r");
+    Message("\t\t   ********************************************\n\r");
+    Message("\t\t        CC3200 SPI Demo Application  \n\r");
+    Message("\t\t   ********************************************\n\r");
+    Message("\n\n\n\r");
+
+    //
+    // Reset the peripheral
+    //
+    MAP_PRCMPeripheralReset(PRCM_GSPI);
+
+#if MASTER_MODE
+
+    MasterMain();
+
+#else
+    printf("nothing here\n");
+
+#endif
+
+    while(1)
+    {
+
+    }
+
+}
+
